9b5556323790a3133ff6142c2522e549c7dab8c4
Kanysh <kanyshomirzak@gmai.com>
2026-04-30 19:16:31 +0500

    Add grouped API configurations for Pirate, Ship, Team, Fleet, Island, and All APIs in Swagger

--- src/main/java/com/hush0k/pirateTeam/config/SwaggerConfig.java
+++ src/main/java/com/hush0k/pirateTeam/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.hush0k.pirateTeam.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,5 +21,53 @@ public class SwaggerConfig {
                                 .bearerFormat("JWT")
                         )
                 );
+    }
+
+    @Bean
+    public GroupedOpenApi pirateApi() {
+        return GroupedOpenApi.builder()
+                .group("Pirate API")
+                .pathsToMatch("/api/pirates/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi shipApi() {
+        return GroupedOpenApi.builder()
+                .group("Ship API")
+                .pathsToMatch("/api/ships/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi teamApi() {
+        return GroupedOpenApi.builder()
+                .group("Team API")
+                .pathsToMatch("/api/teams/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi fleetApi() {
+        return GroupedOpenApi.builder()
+                .group("Fleet API")
+                .pathsToMatch("/api/fleets/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi islandApi() {
+        return GroupedOpenApi.builder()
+                .group("Island API")
+                .pathsToMatch("/api/islands/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi allApi() {
+        return GroupedOpenApi.builder()
+                .group("All API")
+                .pathsToMatch("/api/**")
+                .build();
     }
 }
